32f23ec2050bf9d975ae0661d6fdc44184d3ba03
Jose Reynoso <99152614+D4rk61@users.noreply.github.com>
2023-09-19 08:43:27 -0600

    temp 2

--- src/main/java/ecommerce/web/controller/AdminController.java
+++ src/main/java/ecommerce/web/controller/AdminController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/admin")
+ // @RequestMapping("/admin") original
+@RequestMapping("/")
 public class AdminController {
 
     @GetMapping
